cfaddb92b3756cfa1ffa93491e7e6e6e46cd7ecc
BIGfoot496 <elijah.beregovsky@gmail.com>
2023-02-06 17:41:41 +0300

    changed all tabs to spaces

--- arguments.py
+++ arguments.py
@@ -1,25 +1,27 @@
 """
-	This file contains the arguments to parse at command line.
-	File main.py will call get_args, which then the arguments
-	will be returned.
+    This file contains the arguments to parse at command line.
+    File main.py will call get_args, which then the arguments
+    will be returned.
 """
 import argparse
 
 def get_args():
-	"""
-		Description:
-		Parses arguments at command line.
-		Parameters:
-			None
-		Return:
-			args - the arguments parsed
-	"""
-	parser = argparse.ArgumentParser()
+    """
+        Description:
+        Parses arguments at command line.
+        Parameters:
+            None
+        Return:
+            args - the arguments parsed
+    """
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('--mode', dest='mode', type=str, default='train')              # can be 'train' or 'test'
-	parser.add_argument('--actor_model', dest='actor_model', type=str, default='')     # your actor model filename
-	parser.add_argument('--critic_model', dest='critic_model', type=str, default='')   # your critic model filename
+    parser.add_argument('--mode', dest='mode', type=str, default='train')              # can be 'train' or 'test'
+    parser.add_argument('--actor_model', dest='actor_model', type=str, default='')     # your actor model filename
+    parser.add_argument('--critic_model', dest='critic_model', type=str, default='')   # your critic model filename
+    parser.add_argument('--ep', dest='ep', type=int, default='')                       # number of episodes to run
 
-	args = parser.parse_args()
 
-	return args
+    args = parser.parse_args()
+
+    return args
